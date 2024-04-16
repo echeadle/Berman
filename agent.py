1e47879af0406668d62d31a82c31a4c0c72db6d0
echeadle <echeadle@gmail.com>
2024-04-16 11:53:00 -0600

    Removed keys

--- agent.py
+++
@@ -1,9 +0,0 @@
-import pprint
-
-from autogen import AssistantAgent, UserProxyAgent
-
-pprint.pprint(AssistantAgent.DEFAULT_SYSTEM_MESSAGE)
-
-print("*" * 50)
-
-pprint.pprint(UserProxyAgent.DEFAULT_SYSTEM_MESSAGE)
